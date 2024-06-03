4e22efa64aa7b7329b69cb84b86cbaae9d4e6980
acute <awaisraobuddy@gmail.com>
2024-06-03 09:48:37 +0500

    register Functionality

--- register.php
+++ register.php
@@ -4,10 +4,38 @@
 require 'connection.php';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST' ){
-    $email = $_POST['email'];
-    $password = md5($_POST['password']);
 
-    echo $password;
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $enc_password = md5($password);
+
+    if(!empty($email) && !empty($password)){
+        
+        $check_user_query = "SELECT email FROM users WHERE email = '$email' LIMIT 1";
+        $check_user_query_run = mysqli_query($conn, $check_user_query);
+
+        if(mysqli_num_rows($check_data_query_run) > 0){
+            echo "Email already exist.";
+        }
+        else{
+            $create_user_query = "INSERT INTO users (email, password) VALUES ('$email', '$enc_password')";
+            $create_user_query_run = mysqli_query($conn, $create_user_query);
+
+            if($create_user_query_run){
+                echo "registered Successfully";
+            }
+            else{
+                echo "Something Went wrong";
+            }
+
+        }
+
+    }
+    else{
+        echo "Email or password cannot be empty";
+    }
+
+    
 
 }
 
